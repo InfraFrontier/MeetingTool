27236f6f00e4a89ddebabed0c43259fad2c04523
koscieln <koscieln@210ab51a-7514-0410-b6e0-de9c396846df>
2013-07-10 11:11:23 +0000

    3 other events added.

--- src/main/sql/events.sql
+++ src/main/sql/events.sql
@@ -1,4 +1,11 @@
 
 
 insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
-values("INFRAFRONTIER, IMPC & IKMC Rome Meeting 2013", 	"02.12.2013, 13:00", 	"03.12.2013, 17:30", "CNR, Rome, Italy", 1, "INFRAFRONTIER will cover travel and accomodation for the speakers and for two participants per INFRAFRONTIER and IMPC partner", "")
+values("INFRAFRONTIER, IMPC & IKMC Rome Meeting 2013", "02.12.2013, 13:00", "03.12.2013, 17:30", 
+"CNR, Rome, Italy", 1, "INFRAFRONTIER will cover travel and accomodation for the speakers and for two participants per INFRAFRONTIER and IMPC partner", "romeMeeting");
+
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
+values("IMPC Phenotyping Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "By invitation only", "romeInfrafrontierWorkshop");
+
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
+values("EUCOMMTOOLS Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "The workshop is open to all participants, however the related costs will be covered only for EUCOMMTOOLS delegates and the workshop speakers.", "romeEucommWorkshop");
