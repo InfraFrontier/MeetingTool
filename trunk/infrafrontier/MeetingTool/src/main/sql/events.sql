647ccfb189e94526ca2940ece079d93b3a1e4302
koscieln <koscieln@210ab51a-7514-0410-b6e0-de9c396846df>
2013-10-17 21:59:08 +0000

    add contact info in the DDL

--- trunk/infrafrontier/MeetingTool/src/main/sql/events.sql
+++ trunk/infrafrontier/MeetingTool/src/main/sql/events.sql
@@ -1,15 +1,15 @@
 
-insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
 values("INFRAFRONTIER-I3 Project Meeting, Athens", "18.11.2013", "19.11.2013", 
-"Divani Apollon Palace, Athens", 1, "Please book accommodation directly with Divani Apollon", "athenInfrafrontierMeeting");
+"Divani Apollon Palace, Athens", 1, "Please book accommodation directly with Divani Apollon", "athenInfrafrontierMeeting", "michael.hagn@helmholtz-muenchen.de");
 
-insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
-values("EUCOMMTOOLS Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "The workshop is open to all participants, however the related costs will be covered only for EUCOMMTOOLS delegates and the workshop speakers.", "romeEucommWorkshop");
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
+values("EUCOMMTOOLS Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "The workshop is open to all participants, however the related costs will be covered only for EUCOMMTOOLS delegates and the workshop speakers.", "romeEucommWorkshop", "cornelia.kaloff@helmholtz-muenchen.de");
 
-insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
-values("IMPC Phenotyping Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "By invitation only", "romeInfrafrontierWorkshop");
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
+values("IMPC Phenotyping Workshop", "04.12.2013", "04.12.2013", "CNR, Rome, Italy", 1, "By invitation only", "romeInfrafrontierWorkshop", "h.gates@har.mrc.ac.uk");
 
-insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form) 
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
 values("INFRAFRONTIER, IMPC & IKMC Rome Meeting 2013", "02.12.2013, 13:00", "03.12.2013, 17:30", 
-"CNR, Rome, Italy", 1, "INFRAFRONTIER will cover travel and accomodation for the speakers and for two participants per INFRAFRONTIER and IMPC partner", "romeMeeting");
+"CNR, Rome, Italy", 1, "INFRAFRONTIER will cover travel and accomodation for the speakers and for two participants per INFRAFRONTIER and IMPC partner", "romeMeeting", "ana.decastro@helmholtz-muenchen.de");
 
