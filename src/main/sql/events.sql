e3695159c4f54f204aeedca6fd59c3847063114a
koscieln <koscieln@210ab51a-7514-0410-b6e0-de9c396846df>
2014-01-30 13:33:34 +0000

    fixed a few typos.

--- src/main/sql/events.sql
+++ src/main/sql/events.sql
@@ -15,5 +15,5 @@ values("INFRAFRONTIER, IMPC & IKMC Rome Meeting 2013", "02.12.2013, 13:00", "03.
 
 insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
 values("INFRAFRONTIER/IMPC Workshop: Promoting the international exchange of mouse mutant resources", 
-"08.05.2014", "09.05.2014", "Sheraton München Airport Hotel, Munich, Germany", 1, "", "munichWorkshop", "ana.decastro@helmholtz-muenchen.de");
+"08.05.2014", "09.05.2014", "Sheraton Airport Hotel, Munich, Germany", 1, "", "munichWorkshop", "ana.decastro@helmholtz-muenchen.de");
 
