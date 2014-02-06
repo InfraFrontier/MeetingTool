de3d611b0a382c6c551e755ef36740ab9228efd3
koscieln <koscieln@210ab51a-7514-0410-b6e0-de9c396846df>
2014-02-06 09:15:55 +0000

    new meeting

--- src/main/sql/events.sql
+++ src/main/sql/events.sql
@@ -14,6 +14,10 @@ values("INFRAFRONTIER, IMPC & IKMC Rome Meeting 2013", "02.12.2013, 13:00", "03.
 "CNR, Rome, Italy", 0, "INFRAFRONTIER will cover travel and accomodation for the speakers and for two participants per INFRAFRONTIER and IMPC partner", "romeMeeting", "ana.decastro@helmholtz-muenchen.de");
 
 insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
+values("INFRAFRONTIER Management Meeting", 
+"26.03.2014", "26.03.2014", "Municon Conference Center , Munich, Germany", 1, "", "managementWorkshop", "ana.decastro@helmholtz-muenchen.de");
+
+insert into events(event_name,event_start,event_end,event_place,event_is_active,event_comment,event_form, event_contact) 
 values("INFRAFRONTIER/IMPC Workshop: Promoting the international exchange of mouse mutant resources", 
 "08.05.2014", "09.05.2014", "Sheraton Airport Hotel, Munich, Germany", 1, "", "munichWorkshop", "ana.decastro@helmholtz-muenchen.de");
 
