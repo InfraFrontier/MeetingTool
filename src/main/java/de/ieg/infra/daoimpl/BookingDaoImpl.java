419c0f7bb6b25789c53be2f3873cec0f8b782efa
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2015-06-16 14:58:20 +0000

    Addition of line to bean to add booking to database

--- src/main/java/de/ieg/infra/daoimpl/BookingDaoImpl.java
+++ src/main/java/de/ieg/infra/daoimpl/BookingDaoImpl.java
@@ -12,6 +12,7 @@ import de.ieg.infra.domain.ManagementWorkshopBooking;
 import de.ieg.infra.domain.MunichWorkshopBooking;
 import de.ieg.infra.domain.PragueWorkshopBooking;
 import de.ieg.infra.domain.BarcelonaWorkshopBooking;
+import de.ieg.infra.domain.IpadMdKoWorkshopBooking;
 import de.ieg.infra.domain.MarseilleWorkshopBooking;
 
 public class BookingDaoImpl extends JdbcTemplate implements BookingDao{
@@ -206,6 +207,40 @@ public void insertMarseilleWorkshopBooking(Integer eventId, MarseilleWorkshopBoo
         "" +
         ") "
         +"VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+	update(sql,new Object [] {eventId,booking.getFirstName(),booking.getLastName(),booking.getEmail(),booking.getRemarks(),booking.getInstitution(),booking.getCountry(),booking.getSex(),booking.getAccommodationYesNo(),
+			booking.getDinnerYesNo(),booking.getFlightNeeded(),booking.getOutboundFlightFrom(),booking.getOutboundFlightTo(),booking.getOutboundFlightDate(),booking.getOutboundFlightTime(),
+			booking.getReturnFlightFrom(),booking.getReturnFlightTo(),booking.getReturnFlightDate(),booking.getReturnFlightTime()});
+}
+
+    @Override
+    public void insertIpadMdKoWorkshopBooking(Integer eventId, IpadMdKoWorkshopBooking booking) throws DataAccessException{
+            System.out.println("the method to insert booking has been called");
+		String sql="INSERT INTO bookings ("
+        +"booking_event,"
+		+"booking_first_name,"
+        +"booking_second_name,"
+        +"booking_email,"
+        +"booking_remarks,"
+        +"booking_institution,"
+        +"booking_country," +
+        "booking_sex," +
+        "booking_hotel_needed,"
+        +"booking_attend_dinner,"
+        +"booking_flight_needed," +
+        "booking_outbound_flight_from," +
+        "booking_outbound_flight_to," +
+        "booking_outbound_flight_date," +
+        "booking_outbound_flight_time," +
+        "booking_return_flight_from," +
+        "booking_return_flight_to," +
+        "booking_return_flight_date," +
+        "booking_return_flight_time" +
+        "" +
+        ") "
+        +"VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                
+                System.out.println("IPAD MEETING BOOKING SQL IS :: " + sql);
 
 	update(sql,new Object [] {eventId,booking.getFirstName(),booking.getLastName(),booking.getEmail(),booking.getRemarks(),booking.getInstitution(),booking.getCountry(),booking.getSex(),booking.getAccommodationYesNo(),
 			booking.getDinnerYesNo(),booking.getFlightNeeded(),booking.getOutboundFlightFrom(),booking.getOutboundFlightTo(),booking.getOutboundFlightDate(),booking.getOutboundFlightTime(),
