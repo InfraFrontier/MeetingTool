adb30e068cd0b059e15a15c96474cee68385b248
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2015-06-16 14:46:38 +0000

    Addition of line to bean to add booking to database

--- trunk/infrafrontier/MeetingTool/src/main/java/de/ieg/infra/dao/BookingDao.java
+++ trunk/infrafrontier/MeetingTool/src/main/java/de/ieg/infra/dao/BookingDao.java
@@ -11,38 +11,39 @@ import de.ieg.infra.domain.ManagementWorkshopBooking;
 import de.ieg.infra.domain.MunichWorkshopBooking;
 import de.ieg.infra.domain.PragueWorkshopBooking;
 import de.ieg.infra.domain.BarcelonaWorkshopBooking;
+import de.ieg.infra.domain.IpadMdKoWorkshopBooking;
 import de.ieg.infra.domain.MarseilleWorkshopBooking;
 
 /**
- * @author steinkamp
- * A dao for booking entities
+ * @author steinkamp A dao for booking entities
  *
  */
 public interface BookingDao {
 
-	/**
-	 * Inserts a new booking for a given event
-	 * @param eventId
-	 * @param booking
-	 * @throws DataAccessException
-	 */
-    
-	public void insertInfrafrontierI3KickOffMeetingBooking(Integer eventId,InfrafrontierI3KickOffMeetingBooking booking) throws DataAccessException;
-	
-	public void insertInfImpcIkmcRome1Booking(Integer eventId, InfImpcIkmcRome1Booking booking) throws DataAccessException;
-	
-	public void insertInfImpcIkmcRomeWorkshopBooking(Integer eventId,InfImpcIkmcRomeWorkshopBooking booking) throws DataAccessException;
+    /**
+     * Inserts a new booking for a given event
+     *
+     * @param eventId
+     * @param booking
+     * @throws DataAccessException
+     */
+    public void insertInfrafrontierI3KickOffMeetingBooking(Integer eventId, InfrafrontierI3KickOffMeetingBooking booking) throws DataAccessException;
 
-	public void insertInfrafrontierI3AthensMeetingBooking(Integer eventId,AthenInfrafrontierBooking booking) throws DataAccessException;
+    public void insertInfImpcIkmcRome1Booking(Integer eventId, InfImpcIkmcRome1Booking booking) throws DataAccessException;
 
-	public void insertMunichWorkshopBooking(Integer eventId,MunichWorkshopBooking booking) throws DataAccessException;
-	
-	public void insertManagementWorkshopBooking(Integer eventId,ManagementWorkshopBooking booking) throws DataAccessException;
-	
-	public void insertPragueWorkshopBooking(Integer eventId,PragueWorkshopBooking  booking) throws DataAccessException;
-        
-        public void insertBarcelonaWorkshopBooking(Integer eventId,BarcelonaWorkshopBooking  booking) throws DataAccessException;
-        
-        public void insertMarseilleWorkshopBooking(Integer eventId,MarseilleWorkshopBooking  booking) throws DataAccessException;
-	
+    public void insertInfImpcIkmcRomeWorkshopBooking(Integer eventId, InfImpcIkmcRomeWorkshopBooking booking) throws DataAccessException;
+
+    public void insertInfrafrontierI3AthensMeetingBooking(Integer eventId, AthenInfrafrontierBooking booking) throws DataAccessException;
+
+    public void insertMunichWorkshopBooking(Integer eventId, MunichWorkshopBooking booking) throws DataAccessException;
+
+    public void insertManagementWorkshopBooking(Integer eventId, ManagementWorkshopBooking booking) throws DataAccessException;
+
+    public void insertPragueWorkshopBooking(Integer eventId, PragueWorkshopBooking booking) throws DataAccessException;
+
+    public void insertBarcelonaWorkshopBooking(Integer eventId, BarcelonaWorkshopBooking booking) throws DataAccessException;
+
+    public void insertMarseilleWorkshopBooking(Integer eventId, MarseilleWorkshopBooking booking) throws DataAccessException;
+
+    public void insertIpadMdKoWorkshopBooking(Integer eventId, IpadMdKoWorkshopBooking booking) throws DataAccessException;
 }
